e42a02e78fb4fa9a4e4b6c43bab143e81ef9c3fa
Ameen Haq <ameen@LittPro.local>
2017-12-17 23:45:11 +0000

    Added Jenkinsfile and complete sbr-control GET tests

---
+++ Jenkinsfile
@@ -0,0 +1,128 @@
+#!groovy
+@Library('jenkins-pipeline-shared@feature/version') _
+
+pipeline {
+    environment {
+
+        BRANCH_DEV = "develop"
+        BRANCH_TEST = "release"
+        BRANCH_PROD = "master"
+
+        DEPLOY_DEV = "dev"
+        DEPLOY_TEST = "test"
+        DEPLOY_PROD = "prod"
+
+        GIT_TYPE = "Github"
+        GIT_CREDS = "github-sbr-user"
+        GITLAB_CREDS = "sbr-gitlab-id"
+
+        ORGANIZATION = "ons"
+        TEAM = "sbr"
+        MODULE_NAME = "sbr-api-sys-test"
+    }
+    options {
+        skipDefaultCheckout()
+        buildDiscarder(logRotator(numToKeepStr: '30', artifactNumToKeepStr: '30'))
+        timeout(time: 30, unit: 'MINUTES')
+        timestamps()
+    }
+    agent any
+    stages {
+        stage('Checkout'){
+            agent any
+            steps{
+                deleteDir()
+                checkout scm
+                stash name: 'app'
+                sh "$SBT version"
+                script {
+                    version = '1.0.' + env.BUILD_NUMBER
+                    currentBuild.displayName = version
+                    env.NODE_STAGE = "Checkout"
+                }
+            }
+        }
+        // bundle all libs and dependencies
+        stage ('Bundle') {
+            agent any
+            when {
+                anyOf {
+                    branch BRANCH_DEV
+                    branch BRANCH_TEST
+                    branch BRANCH_PROD
+                }
+            }
+            steps {
+                script {
+                    env.NODE_STAGE = "Bundle"
+                }
+                colourText("info", "Bundling.... adding application.conf")
+                if (BRANCH_NAME == BRANCH_DEV) {
+                    env.DEPLOY_NAME = DEPLOY_DEV
+                }
+                else if  (BRANCH_NAME == BRANCH_TEST) {
+                    env.DEPLOY_NAME = DEPLOY_TEST
+                }
+                else if (BRANCH_NAME == BRANCH_PROD) {
+                    env.DEPLOY_NAME = DEPLOY_PROD
+                }
+                else {
+                    colourText("info", "No matching branch")
+                }
+                dir('conf') {
+                    git(url: "$GITLAB_URL/StatBusReg/${MODULE_NAME}.git", credentialsId: GITLAB_CREDS, branch: "${BRANCH_DEV}")
+                }
+                // stash name: "zip"
+            }
+        }
+        stage('Testing'){
+            agent any
+            steps {
+                colourText("info", "Building ${env.BUILD_ID} on ${env.JENKINS_URL} from branch ${env.BRANCH_NAME}")
+                script {
+                    sh """
+                        cp conf/${env.DEPLOY_NAME}/application.conf src/test/resources
+                        $SBT clean compile test
+                    """
+//                    stash name: 'compiled'
+                }
+            }
+            post {
+                always {
+                    script {
+                        env.NODE_STAGE = "Testing"
+                    }
+                }
+                success {
+                    colourText("info","Passed all system test for all SBR apis.")
+                }
+                failure {
+                    colourText("warn","Failed in system tests for SBR apis.")
+                }
+            }
+        }
+
+    }
+    post {
+        always {
+            script {
+                colourText("info", 'Post steps initiated')
+                deleteDir()
+            }
+        }
+        success {
+            colourText("success", "All stages complete. Build was successful.")
+            sendNotifications currentBuild.result, "\$SBR_EMAIL_LIST"
+        }
+        unstable {
+            colourText("warn", "Something went wrong, build finished with result ${currentResult}. This may be caused by failed tests, code violation or in some cases unexpected interrupt.")
+            sendNotifications currentBuild.result, "\$SBR_EMAIL_LIST", "${env.NODE_STAGE}"
+        }
+        failure {
+            colourText("warn","Process failed at: ${env.NODE_STAGE}")
+            sendNotifications currentBuild.result, "\$SBR_EMAIL_LIST", "${env.NODE_STAGE}"
+        }
+    }
+}
+
+
--- src/test/scala/system/SystemSpec.scala
+++ src/test/scala/system/SystemSpec.scala
@@ -16,55 +16,72 @@ import util.RequestGenerator
 
 class SystemSpec extends AsyncFlatSpec with Matchers with Status {
 
+  private val UNIT_LIST = List("VAT", "PAYE", "LEU", "CH", "ENT")
+  private val REFERENCE_PERIOD_FORMAT = "yyyyMM"
+  private val DELIMITER = "-"
+
   private val application: Application = new GuiceApplicationBuilder().build
   private val ws: WSClient = application.injector.instanceOf(classOf[WSClient])
   private val request = new RequestGenerator(ws)
   private val config: Config = ConfigFactory.load()
 
-  private val unitList = List("VAT", "PAYE", "LEU", "CH", "ENT")
-  private val REFERENCE_PERIOD_FORMAT = "yyyyMM"
-  private val DELIMITER = "-"
+  private val defaultPeriod = config.getInt("sbr-control-api.yearmonth.period")
+  private val controlBaseUrl = config.getString("sbr-control-api.base.url")
+  private val onsEnterpriseNum = config.getString("sbr-control-api.ons.enterprise.number")
+
+  private def yearMonthConversion(period: Int) = YearMonth.parse(period.toString,
+    DateTimeFormat.forPattern(REFERENCE_PERIOD_FORMAT))
 
 
-  private def yearMonthConversion(period: Int)  = YearMonth.parse(period.toString, DateTimeFormat.forPattern(REFERENCE_PERIOD_FORMAT))
+  behavior of "sbr-api"
 
-//  behavior of "sbr-api"
-//
-//  "A full business name request of ONS" should "return ONS record" in {
-//    val request = init.generator
-//    request.singleGETRequest("").map{ resp =>
-//      resp.json.as[Seq[JsValue]].nonEmpty
-//      (resp.json.as[Seq[JsValue]].head \ "postCode").as[String] shouldEqual "NP10 8XG"
-//      resp.status shouldEqual OK
-//      resp.header("Content-Type") shouldEqual Some("application/json")
-//    }
-//  }
+  /**
+    * GET REQUESTS
+    */
 
-  behavior of "sbr-control"
-
-  "A full business name request of ONS unit details" should "return ONS with children links" in {
-    val baseUrl = config.getString("sbr-control-api.base.url")
-    val enterprise = config.getString("sbr-control-api.ons.enterprise.number")
-    request.singleGETRequest(s"$baseUrl/v1/units/$enterprise").map { resp =>
+  "A full business name request of ONS" should "return ONS record" in {
+    request.singleGETRequest("").map{ resp =>
       resp.json.as[Seq[JsValue]].nonEmpty
-      (resp.json.as[Seq[JsValue]].head \ "id").as[String].toLong shouldEqual enterprise.toLong
-      // has some children matched to list
-      (resp.json.as[Seq[JsValue]].head \ "children").as[JsValue].toString contains unitList
+      (resp.json.as[Seq[JsValue]].head \ "postCode").as[String] shouldEqual "NP10 8XG"
       resp.status shouldEqual OK
       resp.header("Content-Type") shouldEqual Some("application/json")
     }
   }
 
 
-  "An enterprise search using ONS full name" should "return ONS children links and enterprise details" in {
-    val baseUrl = config.getString("sbr-control-api.base.url")
-    val enterprise = config.getString("sbr-control-api.ons.enterprise.number")
-    val period = config.getInt("sbr-control-api.yearmonth.period")
-    val yearMonth = yearMonthConversion(period)
-    request.singleGETRequest(s"$baseUrl/v1/periods/$period/enterprises/$enterprise").map { resp =>
-      (resp.json \ "id").as[Long] shouldEqual enterprise.toLong
+  behavior of "sbr-control"
+
+  /**
+    * GET REQUESTS
+     */
+
+  "A full business name request of ONS unit details" should "GET ONS with children links" in {
+    request.singleGETRequest(s"$controlBaseUrl/v1/units/$onsEnterpriseNum").map { resp =>
+      resp.json.as[Seq[JsValue]].nonEmpty
+      (resp.json.as[Seq[JsValue]].head \ "id").as[String].toLong shouldEqual onsEnterpriseNum.toLong
+      // has some children matched to list
+      (resp.json.as[Seq[JsValue]].head \ "children").as[JsValue].toString contains UNIT_LIST
+      resp.status shouldEqual OK
+      resp.header("Content-Type") shouldEqual Some("application/json")
+    }
+  }
+
+  "A search on all three criterias" should "GET ONS record" in {
+    request.singleGETRequest(s"$controlBaseUrl/v1/periods/$defaultPeriod/types/ENT/units/$onsEnterpriseNum").map { resp =>
+      resp.json.as[Seq[JsValue]].nonEmpty
+      (resp.json.as[Seq[JsValue]].head \ "id").as[String].toLong shouldEqual onsEnterpriseNum.toLong
+      // has some children matched to list
+      (resp.json.as[Seq[JsValue]].head \ "children").as[JsValue].toString contains UNIT_LIST
+      resp.status shouldEqual OK
+      resp.header("Content-Type") shouldEqual Some("application/json")
+    }
+  }
+
+  "An enterprise search using ONS full name" should "GET ONS children links and enterprise details" in {
+    val yearMonth = yearMonthConversion(defaultPeriod)
+    request.singleGETRequest(s"$controlBaseUrl/v1/periods/$defaultPeriod/enterprises/$onsEnterpriseNum").map { resp =>
+      (resp.json \ "id").as[Long] shouldEqual onsEnterpriseNum.toLong
       (resp.json \ "childrenJson").as[Seq[JsValue]].nonEmpty
-      println(yearMonth.getMonthOfYear.toString)
       (resp.json \ "period").as[String] shouldEqual String.join(DELIMITER, yearMonth.getYear.toString,
        "0" + yearMonth.getMonthOfYear.toString)
       (resp.json \ "vars").as[JsValue]
@@ -72,5 +89,38 @@ class SystemSpec extends AsyncFlatSpec with Matchers with Status {
       resp.header("Content-Type") shouldEqual Some("application/json")
     }
   }
+
+  "Version request" should "GET a version listing" in {
+    request.singleGETRequest(s"$controlBaseUrl/version").map { resp =>
+      (resp.json \ "name").as[String] shouldEqual "sbr-control-api"
+      (resp.json \ "scalaVersion").as[String] startsWith "2."
+      (resp.json \ "version").as[String] endsWith "-SNAPSHOT"
+//      (resp.json \ "apiVersion").as[String] endsWith "-SNAPSHOT"
+      resp.status shouldEqual OK
+      resp.header("Content-Type") shouldEqual Some("application/json")
+    }
+  }
+
+  "Health request" should "GET a healthy status of the api" in {
+    request.singleGETRequest(s"$controlBaseUrl/health").map { resp =>
+      (resp.json \ "Status").as[String] shouldEqual "OK"
+      resp.status shouldEqual OK
+      resp.header("Content-Type") shouldEqual Some("application/json")
+    }
+  }
+
+  "Hitting the swagger docs route" should "GET the swagger doc ui to test endpoints" in {
+    request.singleGETRequest(s"$controlBaseUrl/docs").map { resp =>
+      resp.status shouldEqual OK
+      resp.header("Content-Type") shouldEqual Some("application/json")
+    }
+  }
+
+  /**
+    * POST REQUESTS
+    */
+
+
+
 
 }
